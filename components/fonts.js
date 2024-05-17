7ff9334edfe9a513059cfe1128dbbd80a98409c3
Porkypus <porkypus@gmail.com>
2024-05-17 10:53:47 +0100

    fix: add font to styled global

--- components/fonts.js
+++ components/fonts.js
@@ -4,6 +4,7 @@ const Fonts = () => (
     <Global
         styles={`
         @import url('https://fonts.googleapis.com/css?family=Roboto Mono');
+        @import url('https://fonts.googleapis.com/css?family=Ubuntu Mono');
         `}
     />
 )
